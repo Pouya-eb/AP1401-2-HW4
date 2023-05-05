d6421af37e28aa485feef6a282c7acdcbc54fa04
Pouya-eb <Pouyaebrahimi1379@gmail.com>
2023-05-06 00:27:08 +0330

    Updating class CehckingAccount

--- cpp/src/checkingAccount.cpp
+++ cpp/src/checkingAccount.cpp
@@ -7,4 +7,11 @@ CheckingAccount::CheckingAccount(double balance, double fee_)
 }
 bool CheckingAccount::withdraw(double amount)
 {
+    if (amount > this->get_balance()) {
+        std::cout << "There is not enough money to withdraw" << std::endl;
+        return 0;
+    }
+    this->deposit(-(fee + amount));
+    // transactions_.push_back(????)
+    return 1;
 }
